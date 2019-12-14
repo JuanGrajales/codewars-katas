180efef7704e0347caca595b5bf4d9005be2d795
JuanGrajales <grajales.juanfe11@gmail.com>
2019-12-13 19:41:30 -0500

    new kata

---
+++ addSolutions/hungry-seven.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/59e61c577905df540000016b/train/javascript
+
+function hungrySeven(arr) {
+    //   let pos = arr.join('').indexOf('789');
+    //   if(pos === -1) return arr;
+    //   arr.splice(pos, 3, 8,9,7);
+    //   return hungrySeven(arr);
+  }
