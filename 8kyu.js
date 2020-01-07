f4a319180d1b514d53a1408af67a6136baffc7f4
Juan Grajales <grajales.juanfe11@gmail.com>
2020-01-07 15:34:20 -0500

    complete you can't code under pressure kata

--- 8kyu.js
+++ 8kyu.js
@@ -54,3 +54,12 @@ function findLongest(str) {
   }
   return longest
 }
+
+/**
+ * You Can't Code Under Pressure #1
+ * https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+ */
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i*2;
+}
