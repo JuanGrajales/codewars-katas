61e553ece1cbc20c2f582f8c1d5a7d299b935027
Juan Grajales <grajales.juanfe11@gmail.com>
2020-01-09 17:40:26 -0500

    ongoing ticker Kata

--- 6kyu.js
+++ 6kyu.js
@@ -1,10 +1,31 @@
 /**
+ * Ticker INCOMPLETE
+ * https://www.codewars.com/kata/5a959662373c2e761d000183/train/javascript
+ */
+const ticker = (text, width, tick) => {
+  let shortenText = "";
+  let textWithPadding;
+  
+  if(tick === 0){
+    return shortenText.padStart(width, " ");
+  }
+  else if(tick <= width){  
+    shortenText = text.substring(0, tick);
+    return shortenText.padStart(width, " ");
+  }
+  else if(tick > width){  
+    shortenText = text.substring(tick-width, tick);
+    return shortenText.padStart(width, " ");
+  }
+}
+
+
+/**
  * Cat and Mouse - Harder Version
  * https://www.codewars.com/kata/57ee2a1b7b45efcf700001bf/train/javascript
  */
 
 function catMouse(x, j){
-
   let dog = x.indexOf("D");
   let cat = x.indexOf("C");
   let mouse = x.indexOf("m");
