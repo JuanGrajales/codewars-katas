0dd654efb3bc2705d362abf2d523682ddd307dd8
Juan Grajales <grajales.juanfe11@gmail.com>
2020-01-14 16:17:55 -0500

    started new kata

--- 8kyu.js
+++ 8kyu.js
@@ -23,3 +23,11 @@ function positiveSum(arr) {
   
   return sum;
 }
+
+/**
+ * Count the divisors of a number
+ * https://www.codewars.com/kata/542c0f198e077084c0000c2e/train/javascript
+ */
+function getDivisorsCnt(n){
+  
+}
