97cd28c5c3388ad245439093460f0d2333488a61
Juan Grajales <grajales.juanfe11@gmail.com>
2020-01-15 15:35:09 -0500

    new kata levels

--- 8kyu.js
+++ 8kyu.js
@@ -25,21 +25,6 @@ function positiveSum(arr) {
 }
 
 /**
- * Count the divisors of a number
- * https://www.codewars.com/kata/542c0f198e077084c0000c2e/train/javascript
- */
-function getDivisorsCnt(n){
-  let sum = 0;
-  
-  for(let i = 1; i <= n; i++){
-    if(n % i == 0)
-      sum++;
-  }
-  
-  return sum;
-}
-
-/**
  * Squash the bugs
  * https://www.codewars.com/kata/56f173a35b91399a05000cb7/train/javascript
  */
