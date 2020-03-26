bfc8476c811644b9d7fa3972be30e34e0d0b7f4c
JuanGrajales <grajales.juanfe11@gmail.com>
2020-03-25 20:25:14 -0400

    new kata

---
+++ addSolutions/square-every-digit.js
@@ -0,0 +1,10 @@
+// toString - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+// join - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join 
+function squareDigits(num){
+    let string = num.toString();  // turn number to a string
+    let results = [];             // create an array to save the new values of the string
+    for (let i = 0; i < string.length; i++){  // iterate through the string
+        results[i] = string[i] * string[i];   // save the square of the number to the array 
+    }
+    return Number(results.join(''));    // turn the array into a string and then into a number
+  }
