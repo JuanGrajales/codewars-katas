b50b018c76297f0a80fb3c426ed0b601c1a04f65
JuanGrajales <grajales.juanfe11@gmail.com>
2020-04-26 20:11:55 -0400

    new kata

---
+++ addSolutions/recursion.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5694cb0ec554589633000036/train/javascript
+
+const factorial = (n) => {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  };
+  
