c80788829312944e8b3a62c0d07a06fe0453d22c
JuanGrajales <grajales.juanfe11@gmail.com>
2020-05-07 20:08:07 -0400

    new kata

---
+++ addSolutions/will-you-make-it.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+function zeroFuel(distanceToPump, mpg, fuelLeft) {
+    // TODO
+    //   let ourFuel= fuelLeft* mpg
+    //    if(ourFuel>= distanceToPump){
+    //    return true;
+    //    } else{
+    //    return false;
+    //    }
+    return distanceToPump <= fuelLeft * mpg;
+  }
+  
