f0c331a7a1bea2fadfb80fc8d62b37f198a63c65
JuanGrajales <grajales.juanfe11@gmail.com>
2020-05-10 20:06:44 -0400

    new kata

---
+++ addSolutions/watermelon.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55192f4ecd82ff826900089e/train/javascript
+function divide(weight) {
+    //your code here
+    if (weight % 2 === 0 && weight > 2) return true;
+    return false;
+  }
+  
