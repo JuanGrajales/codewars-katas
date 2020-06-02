08367614935e68ef66ecde63551c2a76e5b5cf6e
JuanGrajales <grajales.juanfe11@gmail.com>
2020-06-01 20:00:24 -0400

    new kata

---
+++ addSolutions/get-ascii-value-of-character.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/javascript
+
+function getASCII(c) {
+    // read about ASCII https://en.wikipedia.org/wiki/ASCII
+    return c.charCodeAt();
+  }
+  
