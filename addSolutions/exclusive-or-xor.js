11381dcc26a499869c0c8b3082273c61f56680a1
JuanGrajales <grajales.juanfe11@gmail.com>
2020-06-13 19:58:35 -0400

    new kata

---
+++ addSolutions/exclusive-or-xor.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c/train/javascript
+
+function xor(a, b) {
+    // TODO: Program Me
+    //   let num = 0;
+    //   if(a) {
+    //     num++;
+    //   }
+    //   if(b) {
+    //     num++;
+    //   }
+    //   return num === 1;
+  
+    //   if(a===false & b===false || a===true & b===true) return false;
+    //   else return true
+    return a !== b;
+  }
+  
