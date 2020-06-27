356203885d91232e57967668c2f9cddc4d3291a5
JuanGrajales <grajales.juanfe11@gmail.com>
2020-06-27 10:16:32 -0400

    new kata

---
+++ Misc/lodon-city-hacker.js
@@ -0,0 +1,13 @@
+// 7kyu
+
+function londonCityHacker(journey) {
+    let result = 0;
+    for (let i = 0; i < journey.length; i++) {
+      if (typeof journey[i] == "string") result += 2.4;
+      else {
+        result += 1.5;
+        if (typeof journey[i + 1] == "number") i += 1;
+      }
+    }
+    return `£${result.toFixed(2)}`;
+  }
