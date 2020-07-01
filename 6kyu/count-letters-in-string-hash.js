f24a3ec7c1cf295c99f99ebf3da0ab9a86800c8a
JuanGrajales <grajales.juanfe11@gmail.com>
2020-07-01 11:39:02 -0400

    new kata

--- 6kyu/count-letters-in-string-hash.js
+++ 6kyu/count-letters-in-string-hash.js
@@ -1,19 +1,31 @@
 // https://www.codewars.com/kata/5808ff71c7cfa1c6aa00006d/javascript
 // Challenge: solve with forEach
-// Methods: 
+// Methods: split, reduce, ternary operator
 
 // Solution 1
-function letterCount(s){
-    //your code here
-    let obj = {}
-    s.split('').forEach(letter => obj[letter]? obj[letter]++ : obj[letter] = 1)
-    return obj
+function letterCount(s) {
+  //your code here
+  let obj = {};
+  s.split("").forEach((letter) =>
+    obj[letter] ? obj[letter]++ : (obj[letter] = 1)
+  );
+  return obj;
 }
 
 // Solution 2
-const letterCount = s => s
-    .split('')
-    .reduce((accum, letter) => {
-    accum[letter] ? accum[letter]++ : accum[letter] = 1
-    return accum
-  }, {})
+const letterCount = (s) =>
+  s.split("").reduce((accum, letter) => {
+    accum[letter] ? accum[letter]++ : (accum[letter] = 1);
+    return accum;
+  }, {});
+
+// Solution 2
+const letterCount = (s) =>
+  s
+    .split("")
+    .reduce(
+      (accum, letter) => (
+        accum[letter] ? accum[letter]++ : (accum[letter] = 1), accum
+      ),
+      {}
+    );
---
+++ 6kyu/who-likes-it-switch.js
@@ -0,0 +1,53 @@
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362/javascript
+// Challenge: solve with switch
+// Methods:
+
+// Solution 1
+function likes(names) {
+  // TODO
+  //   console.log(names)
+  //   return names.reduce((accum, curr, i) => {
+  //     names.length === 0? accum = 'no one likes this' :
+  //     return accum
+  //   }, '')
+
+  if (names.length === 0) {
+    return "no one likes this";
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length >= 4) {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+// Solution 2
+function likes(names) {
+  names = names || [];
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+      break;
+    case 1:
+      return names[0] + " likes this";
+      break;
+    case 2:
+      return names[0] + " and " + names[1] + " like this";
+      break;
+    case 3:
+      return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+      break;
+    default:
+      return (
+        names[0] +
+        ", " +
+        names[1] +
+        " and " +
+        (names.length - 2) +
+        " others like this"
+      );
+  }
+}
