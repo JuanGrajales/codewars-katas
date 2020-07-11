b52e9e98e7d099fee82add611112930d8fd75f7d
JuanGrajales <grajales.juanfe11@gmail.com>
2020-07-11 08:34:06 -0400

    new kata solution 1

---
+++ 8kyu/return-negative-ternary.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/javascript
+// Challenge: solve with ternary operator
+// Methods: Math.abs
+
+// Solution 1
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
