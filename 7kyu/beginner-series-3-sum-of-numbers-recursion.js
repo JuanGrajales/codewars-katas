0aab892b8f98549da31b0da4c4705c6ba5bb6188
JuanGrajales <grajales.juanfe11@gmail.com>
2020-07-12 09:06:33 -0400

    new kata solution 2

--- 7kyu/beginner-series-3-sum-of-numbers-recursion.js
+++ 7kyu/beginner-series-3-sum-of-numbers-recursion.js
@@ -12,3 +12,10 @@ function getSum(a, b) {
   );
   return arr;
 }
+
+// Solution 2
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+    max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
+};
