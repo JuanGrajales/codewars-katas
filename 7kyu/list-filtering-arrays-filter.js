70c2b503f8780b602a5a176c596fdeda8d57a8f4
JuanGrajales <grajales.juanfe11@gmail.com>
2020-07-15 12:51:07 -0400

    new kata solution 2

--- 7kyu/list-filtering-arrays-filter.js
+++ 7kyu/list-filtering-arrays-filter.js
@@ -7,3 +7,12 @@ function filter_list(l) {
   // Return a new array with the strings filtered out
   return l.filter((el) => typeof el === "number" && !isNaN(el));
 }
+
+// Solution 2
+function filter_list(l) {
+  let res = [];
+  for (let item = 0; item < l.length; item++) {
+    typeof l[item] === "number" ? res.push(l[item]) : null;
+  }
+  return res;
+}
