8136b0733cec80a0d35b9995ba86b84651ccefba
JuanGrajales <inbox1.juan+github@gmail.com>
2020-07-24 17:38:17 -0400

    new kata solution

---
+++ Misc/traverse-array.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5968fb556875980bd900000f
+function diagonal(arr) {
+  // your code here
+  let res = [];
+  for (let i = 2 * (arr.length - 1); i >= 0; i--) {
+    for (let j = arr.length - 1; j >= 0; j--) {
+      let x = i - j;
+      if (arr[x]) res.push(arr[x][j]);
+    }
+  }
+  return res;
+}
