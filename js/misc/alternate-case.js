251050e4757a3b1f40db24fe873963e6ffaca6eb
JuanGrajales <inbox1.juan+github@gmail.com>
2020-08-27 18:20:41 -0400

    new kata

---
+++ js/misc/alternate-case.js
@@ -0,0 +1,36 @@
+//https://www.codewars.com/kata/57a62154cf1fa5b25200031e
+
+//Solution 1 with for loop
+function alternateCase(s) {
+  const flipLetters = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toLowerCase()) {
+      flipLetters[i] = s[i].toUpperCase();
+    } else {
+      flipLetters[i] = s[i].toLowerCase();
+    }
+  }
+
+  return flipLetters.join("");
+}
+
+//Solution 2 with map
+function alternateCase(s) {
+  return s
+    .split("")
+    .map((elem) => {
+      if (elem === elem.toUpperCase()) {
+        return elem.toLowerCase();
+      } else {
+        return elem.toUpperCase();
+      }
+    })
+    .join("");
+}
+
+//Solution 2 shorthand
+alternateCase = (s) =>
+  [...s]
+    .map((x) => (x === x.toUpperCase() ? x.toLowerCase() : x.toUpperCase()))
+    .join("");
