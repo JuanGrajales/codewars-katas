89c73ee5cc57b99af7a7891809b309847405bfcf
JuanGrajales <inbox1.juan+github@gmail.com>
2020-09-10 09:59:20 -0400

    first-python-kata

---
+++ python/8kyu/multiple.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/50654ddff44f800200000004/python
+
+# Solution 1
+def multiply(a, b):
+  return a * b
+
+# Print to console
+print("Hello Python!")
+
+# Function synstax
+
+def funcName(param1):
+    print(param1)
+    
+funcName("test")
+
